7225f9ca94867d68c1908a185fe14ee4fbdbab83
Scott McClure <mccluresc@gmail.com>
2016-08-07 11:23:26 -0400

    Updating comment style to be compatible with Docco.js

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-/* We choose to represent the word as an array of characters. We could have
- * represented it as a string, but an array of chacters is more suited to our
- * specific use case. This is a great way to introduce the concept of data
- * representation. Ask the students "How many different ways could we represent
- * a word?" A string, an array of strings, an array of characters, an array of
- * numbers representing characters, etc. Talk through the pros and cons of each.
- */
+// # Hangman Project #
+
+// ## Global Variables ##
+
+// We choose to represent the word as an array of characters. We could have
+// represented it as a string, but an array of characters is more suited to our
+// specific use case. This is a great way to introduce the concept of data
+// representation. Ask the students "How many different ways could we represent
+// a word?" A string, an array of strings, an array of characters, an array of
+// numbers representing characters, etc. Talk through the pros and cons of each.
 var word = ['A','B','A','L','O','N','E'];
 
-/* Similar to the dice game, we choose to represent the different possible
- * hangman images as an array of strings to the URLs. Talk to students about the
- * challenge we are trying to solve. "How do we turn a number of misses into an
- * image?" An alternative way to solve this would be to concatenate strings with
- * the number of misses to produce the URL.
- */
-var images = ['Hangman-0.png', 'Hangman-1.png', 'Hangman-2.png', 'Hangman-3.png', 'Hangman-4.png', 'Hangman-5.png', 'Hangman-6.png'];
+// Similar to the dice game, we choose to represent the different possible
+// hangman images as an array of strings to the URLs. Talk to students about the
+// challenge we are trying to solve. "How do we turn a number of misses into an
+// image?" An alternative way to solve this would be to concatenate strings with
+// the number of misses to produce the URL.
+var images = ['Hangman-0.png', 'Hangman-1.png', 'Hangman-2.png',
+              'Hangman-3.png', 'Hangman-4.png', 'Hangman-5.png',
+              'Hangman-6.png'];
 
-/* A key decision is how to store the player's progress in the game. We choose
- * to represent it as an array of the letters guessed. Walk through a game of
- * hangman with the students and have them narrate each step. Ask the game
- * leader questions at each step. "How do you know if the player won?"
- * or "How do you know if the player lost?"
- */
+// A key decision is how to store the player's progress in the game. We choose
+// to represent it as an array of the letters guessed. Walk through a game of
+// hangman with the students and have them narrate each step. Ask the game
+// leader questions at each step. "How do you know if the player won?"
+// or "How do you know if the player lost?"
 var guesses = [];
 
-/* While you could infer the number of misses from the array of guesses, it is
- * easier to keep it in an array. Alternatively, you could keep two arrays: a
- * "correct" array and a "wrong" array. */
+// While you could infer the number of misses from the array of guesses, it is
+// easier to keep it in an array. Alternatively, you could keep two arrays: a
+// "correct" array and a "wrong" array.
 var misses = 0;
 
 function updateWord() {
